3eadf31c56be27a27c8767c82713fad08b58ce7d
pikul8637 <112921623+pikul8637@users.noreply.github.com>
2025-06-29 17:14:02 +0530

    IEmployeeMgmtService.java

--- IEmployeeMgmtService.java
+++ IEmployeeMgmtService.java
@@ -4,4 +4,8 @@ import com.nt.entity.Employee;
 
 public interface IEmployeeMgmtService {
 public Iterable<Employee> showAllEmployees();
+public String registerEmployee(Employee emp);
+public Employee findEmployeeByNo(int no); 
+public String updateEmployee(Employee emp);
+public String removeEmployeeById(int id);
 }
